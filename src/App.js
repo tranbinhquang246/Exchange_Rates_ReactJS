b89b3685929403e2c3feaf569381c9f543413ad6
TBQuang <tbquang.18it2@sict.udn.vn>
2022-05-19 15:32:04 +0700

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,78 @@
 import './App.css';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import { HiSwitchHorizontal } from 'react-icons/hi';
+import React, {useState} from 'react'
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [from, setFrom] = useState('CUP');
+  const [to, setTo] = useState('CUP');
+  const [result, setResult] = useState('');
+
+  const handleClickConvert = () => {
+    if(from === "USD"){
+      if(to === "VND"){
+        setResult(amount*23161.50);
+      }
+      else if(to === "RUB"){
+        setResult(amount*63.16);
+      }
+      else if(to === "USD"){
+        setResult(amount)
+      }
+    }else if(from === "VND"){
+      if(to === "RUB"){
+        setResult(amount*0.0027)
+      }else if(to === "USD"){
+        setResult(amount*0.000043)
+      }else if(to === "VND"){
+        setResult(amount);
+      }
+    }else{
+      if(to === "RUB"){
+        setResult(amount)
+      }else if(to === "USD"){
+        setResult(amount*0.016)
+      }else if(to === "VND"){
+        setResult(amount*369.84);
+      }
+    }
+  }
+
+  const handleClickSwitch = () => {
+    var oldfrom = from;
+    setFrom(to);
+    setTo(oldfrom);
+  } 
+
+  const symbols = ['USD', 'VND', 'RUB']
   return (
-    <div className="App">
-      
+    <div className="App bg-gradient-to-r from-slate-400  to-blue-400 w-screen h-screen
+     flex flex-col items-center justify-center">
+        <b className="text-4xl m-5 text-slate-700">Exchange Rates</b>
+        <div className=" flex flex-col items-center w-1/2 h-2/3 bg-white rounded-xl shadow-2xl">
+            <div className="mb-4 flex flex-col mt-2">
+              <label className="text-lg font-bold text-blue-800">Amount</label>
+              <input type="number" className="border rounded-lg p-1 border-blue-800" onChange={(e) => setAmount(e.target.value)}></input>
+            </div>
+            <div className="mb-4 flex justify-around w-full">
+              <div className="mb-4 flex flex-col mt-2">
+                <label className="text-lg font-bold text-blue-800">From</label>
+                <Dropdown options={symbols} value={from} placeholder={'Select an option'} onChange={(e) => setFrom(e.value)}/>
+              </div>
+              <HiSwitchHorizontal className="mt-10 bg-blue-600 rounded-lg w-7 h-7 cursor-pointer" onClick={() => handleClickSwitch()}/>
+              <div className="mb-4 flex flex-col mt-2">
+                <label className="text-lg font-bold text-blue-800">To</label>
+                <Dropdown options={symbols} value={to} placeholder={'Select an option'} onChange={(e) => setTo(e.value)}/>
+              </div>
+            </div>
+            <button className="mt-12 border rounded-xl  bg-blue-400 p-3" onClick={() => handleClickConvert()}>Convert</button>
+            <div className="mt-4 flex flex-col">
+              <label>Result: </label>
+              <strong>{amount} {from} = {result} {to}</strong>
+            </div>
+        </div>
     </div>
   );
 }
